928a7dcb33677dca2333d438914fa286068bacc8
ayesh3p2 <ayesh3p2@gmail.com>
2026-03-08 15:59:07 +0530

    new changes in url

--- services/identity-service/prisma.config.ts
+++ services/identity-service/prisma.config.ts
@@ -3,4 +3,9 @@ import { defineConfig } from 'prisma/config'
 export default defineConfig({
   earlyAccess: true,
   schema: 'prisma/schema.prisma',
+  datasources: {
+    db: {
+      url: process.env.DATABASE_URL!,
+    },
+  },
 })
